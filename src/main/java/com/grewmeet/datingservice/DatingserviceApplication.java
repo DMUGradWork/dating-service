08f5b4823cb8ee87198c7baf89b8db0cd6fcb5a4
sytTest <syt1128@naver.com>
2025-04-03 14:28:25 +0900

    inti: Initial commit

---
+++ src/main/java/com/grewmeet/datingservice/DatingserviceApplication.java
@@ -0,0 +1,13 @@
+package com.grewmeet.datingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatingserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatingserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grewmeet/datingservice/DatingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grewmeet.datingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatingserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
